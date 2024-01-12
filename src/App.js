34412d8b39ee3e4f26a81230f803c1dfc57f0fe5
Kayla Weaver <kaylabeth1211@gmail.com>
2024-01-11 19:54:09 -0800

    app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+function App(){
+  const flavor = "Strawberry";
+  const flavor2 = "Basil";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h1>Yummy yummy strawberry</h1>
+      <h2>One billion  per scoop</h2>
+      <h3>{flavor} and {flavor2} ice creamy goodness</h3>
+      <p><em>All vegan, all the time ;) </em></p>
+      <hr/>
+    </React.Fragment>
   );
 }
 
